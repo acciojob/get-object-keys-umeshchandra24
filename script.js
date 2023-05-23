27bc41ad8935017b3f1258cc32a431a88cd24812
umeshchandra24 <120272310+umeshchandra24@users.noreply.github.com>
2023-05-23 06:32:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 //your JS code here. If required.
-let key = document.getElementById("keys");
 const student = {
 	name : "umesh",
 	getKeys: function(){
@@ -7,4 +6,4 @@ const student = {
 	}
 	
 }
-document.write(student.getKeys());
+student.getKeys();
