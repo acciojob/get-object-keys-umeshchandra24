e6a1a4a8390f73fc0de99d9d52811a49d98f0b09
umeshchandra24 <120272310+umeshchandra24@users.noreply.github.com>
2023-05-23 06:22:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let key = document.getElementById("keys");
+const student = {
+	name : "umesh",
+	getKeys: function(){
+		return Object.keys(student);
+	}
+	
+}
+key.innerText = Object.keys(getKeys());
